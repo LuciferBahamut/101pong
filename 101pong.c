f11b2cee672edb473605b0d70ba64014ad8d728e
Sh4llower <sh4llower@localhost.localdomain>
2019-11-06 15:23:10 +0100

    byte

--- 101pong.c
+++ 101pong.c
@@ -4,20 +4,19 @@
 ** File description:
 ** why 101?
 */
-#include <stdio.h>
 void my_print_coordinates(float x0, float y0, float z0, float vx, float vy, float vz, int n)
 {
-    float xn;
-    float yn;
-    float zn;
+    float xn = x0 + (n * vx);
+    float yn = y0 + (n * vy);
+    float zn = z0 + (n * vz);
 }
 
 void my_print_vector(float x0, float y0, float z0, float x1, float y1, float z1)
 {
-    float vx;
-    float vy;
-    float vz;
-    my_print_coordinates()
+    float vx = x1 - x0;
+    float vy = y1 - y0;
+    float vz = z1 - z0;
+    my_print_coordinates(x0, y0, z0, vx, vy, vz);
 }
 
 void pong(char x0, char y0, char z0, char x1, char y1, char z1, char n)
@@ -29,7 +28,7 @@ int main (int ac, char **av)
 {
     if (ac == 8) {
         pong (av[1], av[2], av[3], av[4], av[5], av[6], av[7])
-        
+            return (0);
     }
-    return(84);
+    return (84);
 }
