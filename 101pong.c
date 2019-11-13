d6c857422d1905285270d37c43d7d66a5dd67a73
Sh4llower <sh4llower@localhost.localdomain>
2019-11-13 11:16:10 +0100

    my_floatoi added

--- 101pong.c
+++ 101pong.c
@@ -5,7 +5,7 @@
 ** i hope i'll be usefull this time
 */
 #include <stdio.h>
-
+#include <stdlib.h>
 int my_atoi(char *str);
 
 float my_floatoi(char *str);
@@ -15,6 +15,7 @@ void my_print_coordinates(float x0, float y0, float z0, float vx, float vy, floa
     float xn = x0 + (n * vx);
     float yn = y0 + (n * vy);
     float zn = z0 + (n * vz);
+
     printf("xn = %f, yn = %f, zn = %f\n", xn, yn, zn);
 }
 
@@ -23,16 +24,16 @@ void my_print_vector(float x0, float y0, float z0, float x1, float y1, float z1,
     float vx = x1 - x0;
     float vy = y1 - y0;
     float vz = z1 - z0;
+
     printf("vx = %f, vy = %f, vz = %f\n", vx, vy, vz);
     my_print_coordinates(x0, y0, z0, vx, vy, vz, n);
 }
 
 void pong(char *x0, char *y0, char *z0, char *x1, char *y1, char *z1, char *n)
 {
-    //non fonctionnel
-    //necessite une conversion char vers float
     int n2 = my_atoi(n);
-    my_print_vector((float)x0, (float)y0, (float)z0, (float)x1, (float)y1, (float)z1, n2);
+
+    my_print_vector(strtod(x0), y0, z0, x1, y1, z1, n2);
 }
 
 int main (int ac, char **av)
--- fonctions.c
+++ fonctions.c
@@ -31,7 +31,41 @@ int my_atoi(char *str)
     }
     return (nb);
 }
-int my_floatoi(char *str)
+int my_floatoi1(char *str)
 {
-    
+    int nb = 0;
+    int n = 0;
+    while (str[n] != ',') {
+        if (str[n] >= '0' && str[n] <= '9') {
+            nb *= 10;
+            nb += str[n] - '0';
+        } else {
+            return(nb)
+        }
+        n++;
+    }
+    return (nb);
+}
+float my_floatoi2(char *str)
+{
+    float nb = 0;
+    int n;
+    for (n = 0; str[n] != ','; n++) {
+    }
+    n++;
+    while (str[n] != '\0') {
+        if (str[n] >='0' && str[n] <= '9') {
+            nb / 10;
+            nb += str[n] - '0';
+        } else {
+            return (nb);
+        }
+    }
+}
+float my_floatoi(char *str)
+{
+    float a = my_floatoi1(*str);
+    float b = my_floatoi2(*str);
+    a += b;
+    return (a);
 }
